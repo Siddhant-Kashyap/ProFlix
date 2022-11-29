7cb14376774deb5554f7b60aba30430c6571f322
Siddhant Kashyap <86836609+Siddhant-Kashyap@users.noreply.github.com>
2022-11-29 18:21:07 +0530

    data added

---
+++ src/Data/EnglishCreator.js
@@ -0,0 +1,25 @@
+export const JSCreator =[
+    {
+        name:"freeCodeCamp",link:"https://youtu.be/jS4aFq5-91M"
+    },
+    {
+        name:"Simpli Learn",link:"https://youtu.be/_zPNLidkNYk"
+    },
+    {
+        name:"Clever Prograammer",link:"https://youtu.be/lI1ae4REbFM"
+    }
+
+]
+
+export const JavaCretor =[
+    {
+        name:"Edureka ",link:"https://youtu.be/hBh_CC5y8-s"
+    },
+    {
+        name:"Simplilearn",link:"https://youtu.be/CFD9EFcNZTQ"
+    },
+    {
+        name:"Telusko",link:"https://youtu.be/8cm1x4bC610"
+    }
+]
+
---
+++ src/Data/HindiCreator.js
@@ -0,0 +1,46 @@
+export const JSCreator =[
+    {
+        name:"Code With Harry",link:"https://www.youtube.com/playlist?list=PLu0W_9lII9ahR1blWXxgSlL4y9iQBnLpR"
+    },
+    {
+        name:"Harshit vashisth",link:"https://www.youtube.com/watch?v=chx9Rs41W6g&list=PLwgFb6VsUj_n15Cg_y2ULKfsOR1XiQqPx&ab_channel=Harshitvashisth"
+    },
+    {
+        name:"Thapa Technical",link:"https://youtu.be/KGkiIBTq0y0"
+    }
+
+]
+
+export const JavaCretor =[
+    {
+        name:"Code With Harry ",link:"https://youtube.com/playlist?list=PLu0W_9lII9agS67Uits0UnJyrYiXhDS6q"
+    },
+    {
+        name:"Anuj Bhaiya",link:"https://youtu.be/aQatrXw0njs"
+    },
+    {
+        name:"Apna College",link:"https://youtube.com/playlist?list=PLfqMhTWNBTe0gqgFk-CUE-ktO5Cek1GdP"
+    }
+]
+
+export const PythonCreator=[
+    {
+        name:"WsCube Tech",link:"https://youtube.com/playlist?list=PLjVLYmrlmjGcQfNj_SLlLV4Ytf39f8BF7"
+
+    },
+    {
+        name:"Code with Harry",link:"https://youtube.com/playlist?list=PLu0W_9lII9agwh1XjRt242xIpHhPT2llg"
+    },
+    {
+        name:"Harshit vashisth",link:"https://youtube.com/playlist?list=PLwgFb6VsUj_lQTpQKDtLXKXElQychT_2j"
+    }
+]
+
+export const  CppCreator =[
+    {
+        name:"Great Learning",link:"https://youtu.be/KC47C8sg2zQ"
+    },
+    {
+        name:"Code it Up",link:"https://youtube.com/playlist?list=PLmRclvVt5DtlXMvqL0zDqQu9Cvy6rsmd4"
+    }
+]
